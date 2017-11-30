4383b722c984c3ccdd776b2d8a4811066e12d498
mirzafarangi <mirzafarangii@gmail.com>
2017-11-30 16:09:42 +0100

    random - fixed
    random - fixed effect model edition

--- app.py
+++ app.py
@@ -153,15 +153,21 @@ def upload_file():
                 g=d*(1-(3/(4*(n_c+n_a)-9)))
                 return g
 
+            def i(n_c,n_a):
+                i=(1-(3/(4*(n_c+n_a)-9)))
+                return i
 
-            s_per_study=SE(df['Group1-sd'],df['Group1-sample size'],df['Group2-sd'],df['Group2-sample size'])
+            s_per_study=SE(df['Sd1'],df['N1'],df['Sd2'],df['N2'])
 
-            d_per_study=abs(d(s_per_study,df['Group2-mean'],df['Group1-mean']))
+            d_per_study=abs(d(s_per_study,df['Mean1'],df['Mean2']))
 
-            g_per_study=g(d_per_study, df['Group1-sample size'], df['Group2-sample size'])
+            g_per_study=g(d_per_study, df['N1'], df['N2'])
 
-            n=df['Group1-sample size']+df['Group2-sample size']
-            n_1=(1/df['Group1-sample size'])+(1/df['Group2-sample size'])
+            i_per_study=i(df['N1'], df['N2'])
+
+
+            n=df['N1']+df['N2']
+            n_1=(1/df['N1'])+(1/df['N2'])
 
             se_d_per_study=((n_1+(d_per_study**2/(2*n))))**0.5
             se_g_per_study=se_d_per_study*(1-3/(4*(n)-9))
@@ -175,8 +181,29 @@ def upload_file():
             w_s_d=1/se_d_per_study**2
             d_s=w_s_d*d_per_study
 
-            w_s_g=1/se_g_per_study**2
-            g_s=w_s_g*g_per_study
+            w_s_g_fixed=1/se_g_per_study**2
+            g_s_fixed=w_s_g_fixed*g_per_study
+            g2_s_fixed=w_s_g_fixed*g_per_study**2
+
+
+            qq=np.sum(g2_s_fixed)-((np.sum(g_s_fixed))**2/np.sum(w_s_g_fixed))
+            c=np.sum(w_s_g_fixed)-((np.sum(w_s_g_fixed**2))/(np.sum(w_s_g_fixed)))
+            degf= len(df.index)-2
+            if qq >= degf:
+                t2=(qq-degf)/c
+            else:
+                t2=0
+
+            w_s_g_random= 1/((se_g_per_study**2)+t2)
+            g_s_random=w_s_g_random*g_per_study
+            g_total_random=np.sum(g_s_random)/np.sum(w_s_g_random)
+            sg_total_random=(1/np.sum(w_s_g_random))**0.5
+
+            lower_g_random=g_total_random-1.96*sg_total_random
+            upper_g_random=g_total_random+1.96*sg_total_random
+
+            g_s_random=w_s_g_random*g_per_study
+
 
 
             d_total=np.sum(d_s)/np.sum(w_s_d)
@@ -185,49 +212,90 @@ def upload_file():
             lower_d=d_total-1.96*s_total
             upper_d=d_total+1.96*s_total
 
-            g_total=np.sum(g_s)/np.sum(w_s_g)
-            sg_total=(1/np.sum(w_s_g))**0.5
+            g_total_fixed=np.sum(g_s_fixed)/np.sum(w_s_g_fixed)
+            sg_total_fixed=(1/np.sum(w_s_g_fixed))**0.5
 
-            lower_g=g_total-1.96*sg_total
-            upper_g=g_total+1.96*sg_total
+            lower_g_fixed=g_total_fixed-1.96*sg_total_fixed
+            upper_g_fixed=g_total_fixed+1.96*sg_total_fixed
 
-            df["d_lower"]=d_lower_per_study
-            df["d"]=d_per_study
-            df["d_upper"]=d_upper_per_study
 
-            df["g_lower"]=g_lower_per_study
-            df["g"]=g_per_study
-            df["g_upper"]=g_upper_per_study
 
-            q=np.sum(w_s_d*d_per_study**2)-((np.sum(d_s)**2)/np.sum(w_s_d))
-            I2=(q-8)/q
-            if q==0:
-                I2=0
+
+            df["Cohen's d"]=d_per_study
+            df["CorrectionFactor"]=i_per_study
+
+            df["Hedges'g (SMD)"]=g_per_study
+            df["SEg"]=se_g_per_study
+
+            df["95%CI-Lower"]= g_lower_per_study
+            df["95%CI-Upper"]= g_upper_per_study
+
+            df["weight(%)-fixed model"]=(w_s_g_fixed/sum(w_s_g_fixed))*100
+            df["weight(%)-random model %"]=(w_s_g_random/sum(w_s_g_random))*100
+
+
+
+
+
+            #fixed model
+
+            q_fixed=np.sum(w_s_g_fixed*g_per_study**2)-((np.sum(g_s_fixed)**2)/np.sum(g_s_fixed))
+            if q_fixed==0:
+                I2_fixed=0
             else:
-                I2=(q-8)/q
+                degf= len(df.index)-2
+                I2_fixed=(q_fixed-degf)/q_fixed
+
+            #random model
+
+            q_random=np.sum(w_s_g_random*g_per_study**2)-((np.sum(g_s_random)**2)/np.sum(g_s_random))
+            if q_random==0:
+                I2_random=0
+            else:
+                degf= len(df.index)-2
+                I2_random=(q_random-degf)/q_random
+
+            df2=pd.DataFrame(index=['Fixed Effect Model','Random Effect Model'], columns=["Hedges's g",'SEg', "95%CI lower", "95%CI upper", 'Heterogeneity %'])
+            df2.xs('Fixed Effect Model')["Hedges's g"]= g_total_fixed
+            df2.xs('Fixed Effect Model')["SEg"]= sg_total_fixed
+            df2.xs('Fixed Effect Model')["95%CI lower"]= lower_g_fixed
+            df2.xs('Fixed Effect Model')["95%CI upper"]= upper_g_fixed
+            df2.xs('Fixed Effect Model')['Heterogeneity %']= I2_fixed
+            df2.xs('Random Effect Model')["Hedges's g"]= g_total_random
+            df2.xs('Random Effect Model')["SEg"]= sg_total_random
+            df2.xs('Random Effect Model')["95%CI lower"]= lower_g_random
+            df2.xs('Random Effect Model')["95%CI upper"]= upper_g_random
+            df2.xs('Random Effect Model')['Heterogeneity %']= I2_random
+
+
+
+            #Save the analysis
 
             df.index+=1
 
             writer = pd.ExcelWriter('results/Meta-Mar_analysis_result.xlsx')
-            df.to_excel(writer,'Sheet1')
+            df.to_excel(writer,'Results per study')
+            df2.to_excel(writer,'Total Results')
+
             writer.save()
 
             study_list = list(map(lambda x: str(x), df['Study'].tolist()))
             resultData = {
                 'study_list': study_list,
-                'd_list': df['d'].tolist(),
-                'd_lower_list': df['d_lower'].tolist(),
-                'd_upper_list': df['d_upper'].tolist(),
+                'g_list': df["Hedges'g (SMD)"].tolist(),
+                'g_lower_list': df["95%CI-Lower"].tolist(),
+                'g_upper_list': df["95%CI-Upper"].tolist(),
                 'total': HTML(df.to_html()),
-                'ave_d': float("{0:.2f}".format(d_total)),
-                'ave_SEd': float("{0:.2f}".format(s_total)),
-                'lower_dd': float("{0:.2f}".format(lower_d)),
-                'upper_dd': float("{0:.2f}".format(upper_d)),
-                'ave_g': float("{0:.2f}".format(g_total)),
-                'ave_SEg': float("{0:.3f}".format(sg_total)),
-                'lower_gg': float("{0:.3f}".format(lower_g)),
-                'upper_gg': float("{0:.3f}".format(upper_g)),
-                'Het': 100 * float("{0:.3f}".format(I2))
+                'ave_g_fixed': float("{0:.2f}".format(g_total_fixed)),
+                'ave_SEg_fixed': float("{0:.2f}".format(sg_total_fixed)),
+                'lower_gg_fixed': float("{0:.2f}".format(lower_g_fixed)),
+                'upper_gg_fixed': float("{0:.2f}".format(upper_g_fixed)),
+                'ave_g_random': float("{0:.2f}".format(g_total_random)),
+                'ave_SEg_random': float("{0:.3f}".format(sg_total_random)),
+                'lower_gg_random': float("{0:.3f}".format(lower_g_random)),
+                'upper_gg_random': float("{0:.3f}".format(upper_g_random)),
+                'Het_fixed': 100 * float("{0:.3f}".format(I2_fixed)),
+                'Het_random': 100 * float("{0:.3f}".format(I2_random))
             }
 
             return render_template("result2.html", **resultData)
