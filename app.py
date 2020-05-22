04bc2e9c74df2b2b83be2d1a76bdc46dd07edd73
mirzafarangi <mirzafarangii@gmail.com>
2020-05-23 01:01:09 +0200

    Revert "email edit"
    This reverts commit 2154ee5530de5ff16d3f284a99107f7e9353bb92.

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ from flask_mail import Mail, Message
 DEBUG = True
 app = Flask(__name__)
 
+#mailjet_rest
+api_key = '7aabd1af2896ce16aa74baaac0350285'
+api_secret = 'f821137c0f5436a262da209cde17127f'
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+
 
 #mail data
 app.config.update(
@@ -2350,15 +2355,28 @@ def submitcontact():
 
 
 
-        #email send
-        msg = Message(subject= "contact",
-                  sender= 'meta.mar00@gmail.com',
-                  recipients=['meta.mar00@gmail.com'])
+        data = {
+        'Messages': [
+        {
+        "From": {
+            "Email": "mirzafarangii@gmail.com",
+            "Name": 'Meta-Mar'
+            },
+            "To": [
+            {
+            "Email": "meta.mar00@gmail.com",
+            "Name": 'Meta-Mar'
+            }
+            ],
+            "Subject": 'contact',
+            "TextPart": "summary of contact:",
+            "HTMLPart": "<b>ip: </b>"+ip_ad+"<p></p>"+"<b>Name: </b>"+your_name+"<p></p>"+"<b> Email: </b>"+your_email+"<p></p>"+"<b>Message: </b>"+your_message
 
-        msg.body= "ip: "+ip_ad+"\n"+"\n Name : "+your_name+"\n"+"\n Email: "+your_email+"\n"+"\n Message:  \n"+"\n"+your_message
-        mail.send(msg)
 
-
+            }
+            ]
+            }
+        result = mailjet.send.create(data=data)
         return render_template("sent.html")
 
 
